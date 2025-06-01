2c638f3827970a5c8241103083ec484c0a4bddea
zxiaosi <2692159310@qq.com>
2025-06-02 02:29:28 +0800

    feat(frontend): 配置 react-compiler

--- frontend/packages/user/vite.config.ts
+++ frontend/packages/user/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// babel.config.js
+const ReactCompilerConfig = {
+  target: '19', // '17' | '18' | '19'
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+  ],
+});
