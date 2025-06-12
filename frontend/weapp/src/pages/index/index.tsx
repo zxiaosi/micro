69887dbcd2179c7002823248da660a62d42beba8
zxiaosi <2692159310@qq.com>
2025-06-12 23:15:34 +0800

    feat(frontend-weapp): 添加 调试微信小程序扫码 登录代码

--- frontend/weapp/src/pages/index/index.tsx
+++ frontend/weapp/src/pages/index/index.tsx
@@ -1,15 +1,16 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.less'
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.less";
 
-export default function Index () {
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+export default function Index() {
+  useLoad((query) => {
+    const scene = decodeURIComponent(query.scene);
+    console.log("Page loaded.", scene);
+  });
 
   return (
-    <View className='index'>
+    <View className="index">
       <Text>Hello world!</Text>
     </View>
-  )
+  );
 }
