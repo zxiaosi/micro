7756b18d11dad04b48fdab329c00bf2d7d0976ac
zxiaosi <2692159310@qq.com>
2025-06-16 01:04:36 +0800

    feat(frontend-web): 添加环境变量

--- frontend/web/packages/main/vite.config.ts
+++ frontend/web/packages/main/vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // babel.config.js
 const ReactCompilerConfig = {
@@ -8,24 +8,42 @@ const ReactCompilerConfig = {
 };
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default ({ mode }) => {
+  // 环境变量文件夹
+  const envDir = resolve(__dirname, 'env');
+  // 加载环境变量
+  const env = loadEnv(mode, envDir);
+
+  return defineConfig({
+    envDir: envDir,
+    server: {
+      port: Number(env.VITE_PORT),
     },
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
       },
     },
-  },
-});
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+    build: {
+      rollupOptions: {
+        // 开排除 react react-dom, 使用 cdn 加载
+        // https://github.com/umijs/qiankun/issues/627
+        external: ['react', 'react-dom'],
+      },
+    },
+  });
+};
