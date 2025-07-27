85a77445c24d6a23d2bf636cdca70a1d58539a0f
zxiaosi <2692159310@qq.com>
2025-07-28 01:32:47 +0800

    feat(frontend-web): main 主应用添加跳转子应用按钮

--- frontend/web/packages/main/src/pages/dashboard/index.tsx
+++ frontend/web/packages/main/src/pages/dashboard/index.tsx
@@ -1,11 +1,19 @@
+import sdk from '@zxiaosi/sdk';
 import './index.less';
 /** 首页 */
 const Dashboard = () => {
   console.log('Dashboard component rendered');
-  
+
   return (
     <div className="dashboard-page">
       <h4>我是主应用 main</h4>
+      <button
+        onClick={() => {
+          sdk.client.navigate?.('/user', { flushSync: true });
+        }}
+      >
+        跳转到 user 子应用
+      </button>
     </div>
   );
 };
