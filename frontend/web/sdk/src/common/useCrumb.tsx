1681a88bcedcf5a89662fa8116b80f1c0b4fe0f3
zxiaosi <2692159310@qq.com>
2025-09-12 01:47:48 +0800

    fix(frontend-web): 获取 面包屑 数据 变为响应式

--- frontend/web/sdk/src/common/useCrumb.tsx
+++ frontend/web/sdk/src/common/useCrumb.tsx
@@ -1,11 +1,15 @@
 import sdk from '@/core';
+import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 
 /**
  * 获取面包屑
  * @see https://reactrouter.com/6.30.1/hooks/use-matches
  */
 const useCrumb = () => {
-  let matches = sdk.client.matches;
+  const location = useLocation();
+
+  let matches = useMemo(() => sdk.client.matches, [location]);
 
   let crumbs = matches
     // @ts-ignore
